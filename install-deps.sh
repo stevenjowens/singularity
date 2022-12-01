035183ea4148fb3ab160b8ece404be3f202a3a06
stevenjowens <stevenjowens@gmail.com>
2022-12-01 00:07:50 -0500

    upgraded from python-opencv to python3-opencv

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install gnustep-gui-runtime cowsay python-opencv
+sudo apt install gnustep-gui-runtime cowsay python3-opencv
 sudo cp robot.cow /usr/share/cowsay/cows/robot.cow
